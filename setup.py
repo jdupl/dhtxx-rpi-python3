7c528492b1ce8c9a0945049eee415d4c544a034b
Justin Duplessis <drfoliberg@gmail.com>
2016-05-16 19:08:13 -0400

    packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='DHT11',
-    packages=['RPi.GPIO']
+    version='0.1.0',
+    description='Library to get results from DHT11 sensor',
+    url='https://github.com/jdupl/dht-python',
+    author='Justin Duplessis',
+    author_email='jdupl@linux.com',
+
+    license='GPLv3',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='dht11 iot raspberrypi rpi pi',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    install_requires=['RPi.GPIO'],
+    extras_require={
+        'test': ['nose'],
+    }
 )
