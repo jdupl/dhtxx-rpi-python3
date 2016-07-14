15f77b166c014fed3478a93d6e0da0f9bfa7a02e
Justin Duplessis <drfoliberg@gmail.com>
2016-07-14 13:51:23 -0400

    update example

--- usage_example.py
+++ usage_example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from time import sleep
-from dht11 import DHT11
+from dhtxx import DHT11, DHT22
 
 
 def read_results_without_exceptions():
@@ -19,11 +19,11 @@ def read_results_without_exceptions():
 
 def read_results_with_exceptions():
     # Adjust pin (BCM) for your needs !
-    dht11 = DHT11(14)
+    dht22 = DHT22(14)
 
     while True:
         try:
-            print(dht11.get_result_once())
+            print(dht22.get_result_once())
         except Exception as e:
             print(e)
         sleep(1)
